57a55d2cba1f9167dbaeb88ccbb740e07cc4f7bd
israfil <israfil45@hotmail.fr>
2022-02-05 17:22:02 +0100

    Branch pour Indah

--- frontend/src/app/apiservice.service.ts
+++ frontend/src/app/apiservice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { ThrowStmt } from '@angular/compiler';
@@ -10,81 +10,70 @@ import { ThrowStmt } from '@angular/compiler';
 })
 export class ApiserviceService {
 
-  constructor(private _http:HttpClient) {}
+  constructor(private _http: HttpClient) { }
 
-    //Connexion du front au back
+  //Connexion du front au back
 
-    apiUrl = 'http://localhost:3000/formations';
+  apiUrl = 'http://localhost:3000/formations';
 
 
-    //récupérer toutes les formations
-    getAllData():Observable<any>
-    {
-      return this._http.get(`${this.apiUrl}`);
-    } 
+  //récupérer toutes les formations
+  getAllData(): Observable<any> {
+    return this._http.get(`${this.apiUrl}`);
+  }
 
-    //créer une formation
-    creerFormation(formation:any):Observable<any> 
-    {
-      console.log(formation, 'createAPI=>');
-      return this._http.post(`${this.apiUrl}`, formation)
-    }
+  //créer une formation
+  creerFormation(formation: any): Observable<any> {
+    console.log(formation, 'createAPI=>');
+    return this._http.post(`${this.apiUrl}`, formation)
+  }
 
-    //supprimer une formation
-    supprimerFormation(id:any):Observable<any>
-    {
-      let ids = id;
-      return this._http.delete(`${this.apiUrl}/${ids}`)
-    }
+  //supprimer une formation
+  supprimerFormation(id: any): Observable<any> {
+    let ids = id;
+    return this._http.delete(`${this.apiUrl}/${ids}`)
+  }
 
-    //updateFormation 
-    updateData(data:any, id:any):Observable<any> 
-    {
-      let ids = id;
-      return this._http.put(`${this.apiUrl}/${ids}`, data);
-    }
+  //updateFormation 
+  updateData(data: any, id: any): Observable<any> {
+    let ids = id;
+    return this._http.put(`${this.apiUrl}/${ids}`, data);
+  }
 
-    //getSingleData
-    getSingleData(id:any):Observable<any> 
-    {
-      let ids = id;
-      return this._http.get(`${this.apiUrl}/${ids}`)
-    }
+  //getSingleData
+  getSingleData(id: any): Observable<any> {
+    let ids = id;
+    return this._http.get(`${this.apiUrl}/${ids}`)
+  }
 
-    // FORMATEUR
+  // FORMATEUR
 
-    apiUrlFormateurs = 'http://localhost:3000/formateurs';
+  apiUrlFormateurs = 'http://localhost:3000/formateurs';
 
-    getAllDataFormateurs():Observable<any>
-    {
-      return this._http.get(`${this.apiUrlFormateurs}`);
-    } 
+  getAllDataFormateurs(): Observable<any> {
+    return this._http.get(`${this.apiUrlFormateurs}`);
+  }
 
-    //Ajouter un formateur
-  ajouterFormateurs(data:any):Observable<any>
-  {
-    
+  //Ajouter un formateur
+  ajouterFormateurs(data: any): Observable<any> {
+
     return this._http.post(`${this.apiUrlFormateurs}`, data);
 
   }
 
   //Supprimer un formateur
-  supprimerFormateurs(id:any):Observable<any>
-  {
+  supprimerFormateurs(id: any): Observable<any> {
     let ids = id;
     return this._http.delete(`${this.apiUrlFormateurs}/${ids}`);
-}
+  }
 
   //Modifier un formateur
-  modifierFormateurs(data:any, id:any):Observable<any>
-
-  {
-    let ids =id;
+  modifierFormateurs(data: any, id: any): Observable<any> {
+    let ids = id;
     return this._http.put(`${this.apiUrlFormateurs}/${ids}`, data);
   }
 
-  getSingleDataFormateur(id:any):Observable<any> 
-  {
+  getSingleDataFormateur(id: any): Observable<any> {
     let ids = id;
     return this._http.get(`${this.apiUrlFormateurs}/${ids}`)
   }
---
+++ frontend/src/app/stagiaires/formations.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-formations',
+  templateUrl: './formations.component.html',
+  styleUrls: ['./formations.component.css']
+
+})
+export class FormationsComponent implements OnInit {
+
+  // constructor(private routeur :RouterOutlet, private routeActive :ActivatedRoute) { }
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
+
+
